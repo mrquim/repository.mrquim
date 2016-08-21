40670a4cbe5abe419b7d74f6dc5962d9e5fdbdc9
Joaquim de Oliveira <misterquim.com@gmail.com>
2016-08-21 18:58:24 +0200

    Add-ons Inspiration e Traquinas adicionados!

---
+++ repo/plugin.video.inspiration/_Edit.py
@@ -0,0 +1,4 @@
+import xbmcaddon
+
+MainBase = 'https://raw.githubusercontent.com/inspirationlinks/inspiration/novo-adoon/plugin.video.inspiration'
+addon = xbmcaddon.Addon('plugin.video.inspiration')
